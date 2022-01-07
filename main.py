270eeed331a2a7405bb810378a7885968fc2ba65
Teyxos <teyxosesp@gmail.com>
2022-01-07 11:11:33 +0100

    Added deposit and logs for saving

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
+from datetime import datetime
+
+
+def trasaction(func):
+    def wrapper(self, amount):
+        with open("./logs.txt", "a") as logs:
+            logs.write(f"Start transaction:{datetime.now()}\t")
+            func(self, amount)
+            logs.write(f"End transaction:{datetime.now()}\n")
+
+    return wrapper
+
+
 class BankAccount:
     def __init__(self, initial_amount):
         try:
             with open("./logs.txt", "r") as file:
                 content = file.read()
-                content = content.split("amount:")
-                self.amount = content[-1]
+                content = content.split("\t")
+                content = content[-3]
+                content = content.split("Amount:")
+                self.amount = content[1]
         except Exception:
             self.amount = initial_amount
 
+    @trasaction
+    def deposit(self, amount):
+        try:
+            self.amount = float(self.amount)
+        except ValueError:
+            pass
 
-user = BankAccount(30.00)
+        total = self.amount + amount
+
+        with open("./logs.txt", "a") as logs:
+            logs.write(f"Deposited:{amount}\t Amount:{total}\t")
+
+        self.amount = total
+
+    def get_amount(self):
+        with open("./logs.txt", "r") as logs:
+            content = logs.read()
+            content = content.split("\t")
+            content = content[-3]
+            content = content.split("Amount:")
+            total = content[1]
+
+        self.amount = total
+
+        return total
+
+
+user = BankAccount(0.00)
+user.deposit(30.00)
+user.deposit(500.00)
